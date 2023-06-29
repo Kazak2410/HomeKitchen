889f984b4dcd8e955f8218c7467ad7f41d83b62e
Kazak <ballukrainian@gmail.com>
2023-06-29 19:17:12 +0300

    several changes in settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('HOMEKITCHEN_SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
